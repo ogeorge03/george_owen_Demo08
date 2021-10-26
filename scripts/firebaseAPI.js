1790209bd973dbb0fd985fd6757670143f25d47a
Owen George <ogeorge5@my.bcit.ca>
2021-10-26 12:18:23 -0700

    login finished

--- scripts/firebaseAPI.js
+++ scripts/firebaseAPI.js
@@ -1,13 +1,13 @@
 // Your web app's Firebase configuration
-var  firebaseConfig = {
-    apiKey: ,
-    authDomain: ",
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId: 
-  };
-  
+var firebaseConfig = {
+  apiKey: "AIzaSyCkzRqTKmJHOk8Oqj3QKDASgQOekCP62t4",
+  authDomain: "demo8owengeorge.firebaseapp.com",
+  projectId: "demo8owengeorge",
+  storageBucket: "demo8owengeorge.appspot.com",
+  messagingSenderId: "707852230167",
+  appId: "1:707852230167:web:1c6edafb9f1eb2725cc652"
+};
 
-  const app = firebase.initializeApp(firebaseConfig);
-  
+
+const app = firebase.initializeApp(firebaseConfig);
+
